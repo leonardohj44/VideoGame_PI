ea078ac3d3b640e45b1a530e2018897a6dd10c0a
leonardo <leonardohj@gmail.com>
2022-09-14 01:07:53 -0500

    proyecto

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,7 +4,7 @@ import thunk from "redux-thunk"; // para poder hacer acciones con promesas
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 export const store = createStore (
-    rootReducer, 
-    composeEnhancer(applyMiddleware(thunk)))
+    rootReducer, composeEnhancer(applyMiddleware(thunk))   // trunk se usa para hacer pedidos asincronos, un pedido a nuestro back
+)
 
 //export default store
