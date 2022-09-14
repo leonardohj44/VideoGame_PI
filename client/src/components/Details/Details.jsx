99e03b0426ea1035342c368b9978ed97b8230016
leonardo <leonardohj@gmail.com>
2022-09-13 23:53:20 -0500

    proyecto

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -5,44 +5,37 @@ import { resetDetails } from '../../action';
 import './Details.css';
 
 export default function Details() {
-    
+
     const dispatch = useDispatch()
     const history = useHistory()
-  
-    
-    let resp = useSelector(state => state.detail) 
 
-    function handleBack(e){
+    let resp = useSelector(state => state.detail)
+
+    function handleBack(e) {
         e.preventDefault()
         dispatch(resetDetails())
         history.push('/home')
     }
-        return (
-            <div >{
-                resp && resp.platforms && resp.genres ? (
-                    <div className="recaja">
-                        <div>
-                        <button className="backDetails"onClick={ (e) => handleBack(e)}>Back</button>
-
-                        </div>
-                        <h2 className="h3_details">Name : {resp.name}</h2>
-                        
-                        <img className="image" src={resp.img} alt="Not found Img" />
-                        
-                    
-                        <p className="p">Rating: {resp.rating}</p>
-                        <p className="p">Genre: {resp.genres !== undefined ?resp.genres.map(g => g.name + (" - ")): "Empty"}</p>
-                      
-                        <p className='conteinDesc'>Description:{resp.description}</p>
-                      
-                        <p className="p">Released: {resp.released}</p>
-                        {
-                             typeof( resp.id) === "string" ?
-                             <p className="p">Platforms: {resp.platforms !== undefined ?resp.platforms.map(p => p.split(" ").join(" - ")): "empty"}</p> :
-                            <p className="p">Platforms: {resp.platforms !== undefined ?resp.platforms.map(p => p.name + (" - ")): "empty"}</p>
-                        }
+    return (
+        <div >{
+            resp && resp.platforms && resp.genres ? (
+                <div className="recaja">
+                    <div>
+                        <button className="backDetails" onClick={(e) => handleBack(e)}>Back</button>
                     </div>
-                    ) : <h1>cargando....</h1>
-            }</div>
-        )
+                    <h2 className="h3_details">Name : {resp.name}</h2>
+                    <img className="image" src={resp.img} alt="Not found Img" />
+                    <p className="p">Rating: {resp.rating}</p>
+                    <p className="p">Genre: {resp.genres !== undefined ? resp.genres.map(g => g.name + (" - ")) : "Empty"}</p>
+                    <p className='conteinDesc'>Description:{resp.description}</p>
+                    <p className="p">Released: {resp.released}</p>
+                    {
+                        typeof (resp.id) === "string" ?
+                            <p className="p">Platforms: {resp.platforms !== undefined ? resp.platforms.map(p => p.split(" ").join(" - ")) : "empty"}</p> :
+                            <p className="p">Platforms: {resp.platforms !== undefined ? resp.platforms.map(p => p.name + (" - ")) : "empty"}</p>
+                    }
+                </div>
+            ) : <h1>cargando....</h1>
+        }</div>
+    )
 }
